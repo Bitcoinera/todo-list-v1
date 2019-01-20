8de6ad5d21eaf74531c4c2bbc06fb0999e55c088
anagutjor <anagjordano@gmail.com>
2019-01-20 18:48:54 +0100

    add custom routes

--- app.js
+++ app.js
@@ -22,16 +22,38 @@ var itemSchema = mongoose.Schema({
 
 const Item = mongoose.model('Item', itemSchema);
 
-var workItemSchema = mongoose.Schema({
-    item: {
+// var workItemSchema = mongoose.Schema({
+//     item: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+const Workitem = mongoose.model('Workitem', itemSchema);
+
+const listSchema = {
+    name: {
         type: String,
         required: true
-    }
+    },
+    items: [itemSchema]
+}
+
+const List = mongoose.model('List', listSchema);
+
+const item1 = new Item({
+    item: "Add your own items"
 })
 
-const Workitem = mongoose.model('Workitem', workItemSchema);
+const item2 = new Item({
+    item: "Delete by checking the checkbox"
+})
+
+const customItems = [item1, item2];
+
 
 app.get("/", function(req, res){
+
     let route = "/work";
 
     let day = date.getDate()
@@ -41,13 +63,51 @@ app.get("/", function(req, res){
     Item.find({}, function(err, items){
         if (err) { throw err} else {
             items.forEach(function(item){
-                dailyItems.push(item.item);
+                dailyItems.push(item);
             })
         };
         
         res.render("list", {listTitle: day, items: dailyItems, route: route});
-    })
+    })  
+})
+
+app.get("/work", function(req, res){
+
+    let route = "/";
+
+    let workItems = [];
+
+    Workitem.find({}, function(err, workitems){
+        if (err) throw err;
+        workitems.forEach(function(workitem){
+            workItems.push(workitem);
+        });
     
+        res.render("list", {listTitle: "Work List", items: workItems, route: route});
+    })
+})
+
+app.get("/:customListName", function(req, res){
+
+    const customListName = req.params.customListName;
+
+    List.findOne({name: customListName}, function(err, foundList){
+        if (err){ throw err} else {
+           
+            if (foundList === null){
+                const list = new List({
+                    name: customListName,
+                    items: customItems
+                })
+            
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+              
+                res.render("list", {listTitle: foundList.name, items: foundList.items, route: "/"});
+            }
+        }
+    })
 })
 
 app.post("/", function(req, res){
@@ -92,23 +152,6 @@ app.post("/delete/work", function(req, res){
         }
         res.redirect("/");
     }) 
-})
-
-app.get("/work", function(req, res){
-
-    let route = "/";
-
-    let workItems = [];
-
-    Workitem.find({}, function(err, workitems){
-        if (err) throw err;
-        workitems.forEach(function(workitem){
-            workItems.push(workitem.item);
-        });
-    
-        res.render("list", {listTitle: "Work List", items: workItems, route: route});
-    })
-    
 })
 
 app.listen(3000, function(){
