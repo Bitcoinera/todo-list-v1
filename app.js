9f39455ae84e292681c82c623de54a3bfe8f26fe
anagutjor <anagjordano@gmail.com>
2019-01-21 16:38:34 +0100

    add navbar with all lists

--- app.js
+++ app.js
@@ -46,11 +46,11 @@ const customItems = [item1, item2];
 
 app.get("/", function(req, res){
 
-    let postroute = "/home";
-
     let day = date.getDate()
     
     let dailyItems = [];
+
+    let listLists = [];
 
     Item.find({}, function(err, items){
         if (err) { throw err} else {
@@ -59,13 +59,22 @@ app.get("/", function(req, res){
             })
         };
         
-        res.render("list", {listTitle: day, items: dailyItems, postroute: postroute});
+        List.find({}, function(err, lists){
+            if (!err) {
+                lists.forEach(function(list){
+                    listLists.push(list.name);
+                })
+            }
+        
+        res.render("list", {listTitle: day, items: dailyItems, lists: listLists});
+        })
     })  
 })
 
 app.get("/:customListName", function(req, res){
 
     const customListName = _.capitalize(req.params.customListName);
+    let listLists = [];
 
     List.findOne({name: customListName}, function(err, foundList){
         if (err){ throw err} else {
@@ -79,8 +88,14 @@ app.get("/:customListName", function(req, res){
                 list.save();
                 res.redirect("/" + customListName);
             } else {
-              
-                res.render("list", {listTitle: foundList.name, items: foundList.items, postroute: "/" + customListName});
+                List.find({}, function(err, lists){
+                    if (!err) {
+                        lists.forEach(function(list){
+                            listLists.push(list.name);
+                        })
+                    }
+                res.render("list", {listTitle: foundList.name, items: foundList.items, lists: listLists});
+                })
             }
         }
     })
