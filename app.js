1f4c390dd2d41f4cd9ad2ba26dabfa60c5fa7570
anagutjor <anagjordano@gmail.com>
2019-02-24 18:13:47 +0100

    get ready for postgresql

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-let newItems= ["Buy Food", "Clean", "Wash dishes"]; 
-let newWorkItems = [];
+let item1 = 'Add any new items you\'d like';
+let item2 = 'Delete the items just by clicking the checkbox'
+const defaultItems= [item1, item2];
 
-app.get("/", function(req, res){
-    let route = "/work";
+app.get('/', function(req, res){
 
     let day = date.getDate()
 
-    res.render("list", {listTitle: day, items: newItems, route: route});
+    res.render('list', {listTitle: day, items: defaultItems});
 })
 
-app.post("/", function(req, res){
+app.post('/', function(req, res){
     let list = req.body.list;
 
-    if (list === "Work"){
+    defaultItems.push(req.body.newItem);
 
-        newWorkItems.push(req.body.newItem);
-        
-        res.redirect("/work");
-    } else {
-
-    newItems.push(req.body.newItem);
-        
-    res.redirect("/");
-    }
+    res.redirect('/');
 })
 
-app.get("/work", function(req, res){
+app.get('/work', function(req, res){
 
-    let route = "/";
-   
-    res.render("list", {listTitle: "Work List", items: newWorkItems, route: route});
+    res.render('list', {listTitle: 'Work List', items: newWorkItems});
+})
+
+app.get('/:newListName', function(req, res){
+    let newListName = req.params.newListName;
+
+    res.render('list', {listTitle: newListName, items: defaultItems});
+})
+
+app.post('/:newListName', function(req, res){
+    let newListName = req.params.newListName;
+
+    defaultItems.push(req.body.newItem);
+
+    res.redirect('/' + newListName);
 })
 
 app.listen(3000, function(){
-    console.log("The server is running on port 3000");
+    console.log('The server is running on port 3000');
 })
