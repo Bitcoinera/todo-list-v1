c6114cdce53c480ec8634dce69ad74a4d4eea536
anagutjor <anagjordano@gmail.com>
2019-02-25 21:57:42 +0100

    add databasePool to create pool

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js');
---
+++ databasePool.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+let config = {
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    host: 'localhost', // Server hosting the postgres database
+    port: process.env.PGPORT,
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+
+const pool = new Pool(config);
+
+module.exports = pool;
+
+pool.query('SELECT * from item', (err, res) => {
+    if (err) return console.log('error: ', err);
+    console.log('res.rows', res.rows);
+    pool.end();
+})
